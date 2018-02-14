256bacfb9f45417f65526090844c92dce847862c
RivinduM <rivindum.15@cse.mrt.ac.lk>
2018-02-14 07:55:10 +0530

    Drag & identify dropped elemnt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {ToolboxComponent} from './toolbox/toolbox.component';
 import {DragAndDropModule} from 'angular-draggable-droppable';
+import {DndModule} from 'ng2-dnd';
+import { InputBoxComponent } from './input-box/input-box.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolboxComponent
+    ToolboxComponent,
+    InputBoxComponent
   ],
   imports: [
     BrowserModule,
-    DragAndDropModule.forRoot()
+    DragAndDropModule.forRoot(),
+    DndModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent, ToolboxComponent]
+  bootstrap: [AppComponent, ToolboxComponent],
+  exports: [BrowserModule, DndModule]
 })
 export class AppModule { }
--- src/app/toolbox/toolbox.component.ts
+++ src/app/toolbox/toolbox.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {InputBoxComponent} from '../input-box/input-box.component';
 
 @NgModule({
   imports: [NgbModule]
@@ -10,14 +11,37 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./toolbox.component.css']
 })
 export class ToolboxComponent implements OnInit {
-
-  constructor() { }
-  droppedData: string;
-
-  dragEnd(event) {
-    console.log('Element was dragged', event);
+  constructor() {
   }
+
   ngOnInit() {
+
   }
 
+
+
+  insertBox() {
+    document.getElementById('canvas').appendChild('<app-input-box></app-input-box>');
+  }
+
+
+
+
+  allowDrop(ev) {
+    ev.preventDefault();
+  }
+
+  drag(ev) {
+    ev.dataTransfer.setData('text', ev.target.id);
+
+  }
+
+  drop(ev) {
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData('text');
+    /*ev.target.appendChild(document.getElementById(data));*/
+    alert(data);
+    this.insertBox();
+  }
 }
+
