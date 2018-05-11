1d9e82c96fbc650669dda2e07e48b52a9d53a607
RivinduM <rivindum.15@cse.mrt.ac.lk>
2018-05-12 00:05:37 +0530

    src index added

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'src/index.html'));
 });
 app.listen(port,() => {
   console.log('Server started on port ' + port);
